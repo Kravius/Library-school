50184efb366e44a85e3d3b83c08c6f69f5ce9f82
Glib Shemenkov <shemenkovglib@gmail.com>
2023-08-13 20:43:18 +0200

    fix: gradient, burger location,

--- library/burger.js
+++ library/burger.js
@@ -1,7 +1,25 @@
 const burger =  document.querySelector('.hamburger');
 const navigation = document.querySelector('.header__navigation');
+const body = document.querySelector('body');
+
 burger.addEventListener('click', function() {
 	this.classList.toggle('active');
 	navigation.classList.toggle('open');
-	document.querySelector('body').classList.toggle('lock');
+	body.classList.toggle('lock');
+})
+
+const navLink = document.querySelectorAll('.navigation__link');
+
+navLink.forEach(link => {
+	link.addEventListener('click', function() {
+		burger.classList.remove('active');
+		navigation.classList.remove('open');
+		body.classList.remove('lock');
+	})
+})
+
+navigation.addEventListener('click', () => {
+	burger.classList.remove('active');
+	navigation.classList.remove('open');
+	body.classList.remove('lock');
 })
