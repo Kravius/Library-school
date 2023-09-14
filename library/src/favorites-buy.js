867361a6e962c707b47aef47b760412ad12e9900
Glib Shemenkov <shemenkovglib@gmail.com>
2023-09-14 16:58:35 +0200

    feat: fix ......

--- library/src/favorites-buy.js
+++ library/src/favorites-buy.js
@@ -3,15 +3,15 @@ import BuyLibraryCard from "./profiles/library-card.js";
 
 export default (function () {
 	const favoritesBtnBuy = document.querySelectorAll('.favorites__book button');
+	const activeMenu = document.querySelector('.active-profile-btn');
 	const user = JSON.parse(localStorage.getItem('users'));
 	const indexUser = JSON.parse(localStorage.getItem('activUserIndex'));
 
 	favoritesBtnBuy.forEach(btn => {
 		btn.addEventListener('click', (ev) => {
-			console.log()
 			//check do we have activ user or not
 			if (user) {
-				if (indexUser === undefined){
+				if (indexUser === null) {
 					let loginMenu = new LoginMenu('');
 					loginMenu.closeButton.addEventListener('click', () => {
 						const removeLoginMenu = ev.target.nextElementSibling;
@@ -28,25 +28,19 @@ export default (function () {
 					}
 
 				}
-
-				let libraryCard = new BuyLibraryCard();
-				document.querySelector('.library-card').classList.add('open')
-			}
-			if (!user) {
-				// let loginMenu = new LoginMenu();
-				// loginMenu.closeButton.addEventListener('click', () => {
-				// 	const removeLoginMenu = ev.target.nextElementSibling;
-				// 	if (removeLoginMenu) {
-				// 		const paretElement = removeLoginMenu.parentNode;
-				// 		paretElement.removeChild(removeLoginMenu);
-				// 	}
-				// })
-				// ev.target.insertAdjacentElement('afterend', loginMenu.container);
-				// const closestContainerLogin = ev.target.nextElementSibling;
-				// console.log(closestContainerLogin)
-				// if (closestContainerLogin) {
-				// 	closestContainerLogin.classList.add('open');
-				// }
+				if (user[indexUser].buyCard === false) {
+					let libraryCard = new BuyLibraryCard();
+					document.querySelector('.library-card').classList.add('open')
+					window.scrollTo(0, 0);
+				}
+				else {
+					user[indexUser].books++;
+					localStorage.setItem('users', JSON.stringify(user));
+					location.reload();
+					// btn.classList.remove('button_colored');
+					// btn.textContent = 'Own';
+					// btn.disabled = true;
+				}
 			}
 		})
 	})
